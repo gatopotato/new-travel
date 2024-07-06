b60746e1d3bdcf653a2d55aca86822ad5f1d5ee2
gatopotato <samkitjai09@gmail.com>
2024-07-06 22:17:52 +0530

    fucked shit pt 2

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ dotenv.config();
 
 app.use(express.json());
 
-app.use(express.static("../dist"));
+app.use(express.static("./frontend/dist"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend" ,"dist", "index.html"))
 });
